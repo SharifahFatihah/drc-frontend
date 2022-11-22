9bb58a65e5a1d82ff667547d54213165b8e466ca
afifi <afifi@besquare.com.my>
2022-11-22 12:56:36 +0800

    resize for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import {
   AppBar,
   Container,
+  MenuItem,
+  Select,
+  ThemeProvider,
   Toolbar,
   Typography,
-  Select,
-  MenuItem,
-  makeStyles,
   createTheme,
-  ThemeProvider,
+  makeStyles,
 } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 
-import { CryptoState } from "../CryptoContext";
 import AuthModal from "./Authentication/AuthModal";
-import UserSidebar from "./UserSidebar";
+import { CryptoState } from "../CryptoContext";
+import HeaderMenu from "./HeaderMenu";
 import LogoIcon from "../asset/logoicon.png";
 import LogoWord from "../asset/logoword.png";
-import HeaderMenu from "./HeaderMenu";
+import UserSidebar from "./UserSidebar";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -67,6 +67,14 @@ function Header() {
   useEffect(() => {
     window.addEventListener("resize", handleResize);
   });
+
+  useEffect(() => {
+    if (window.innerWidth < 800) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, []);
 
   const selectMenu = (
     <Select
