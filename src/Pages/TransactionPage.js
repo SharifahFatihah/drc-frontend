af9a0f9a7c3db51be601a15e7be3bc631a4b67b2
Nic Amir <amir@besquare.com.my>
2022-11-29 09:57:50 +0000

    transaction history finish function

--- src/Pages/TransactionPage.js
+++ src/Pages/TransactionPage.js
@@ -1,6 +1,15 @@
-import { createTheme, makeStyles, ThemeProvider } from "@material-ui/core";
+import {
+  Button,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import { CryptoState } from "../CryptoContext";
+import TransactionTable from "../components/TransactionTable";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -28,9 +37,65 @@ function TransactionPage() {
 
   console.log("transactionpage", receipt);
 
+  const deleteReceipt = async () => {
+    const transactionRef = doc(db, "transaction", user?.uid);
+
+    try {
+      await setDoc(
+        transactionRef,
+        {
+          receipts: [],
+        },
+        { merge: "true" }
+      );
+    } catch (error) {}
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
-      <div className={classes.container}>TransactionPage</div>
+      <div className={classes.container}>
+        <div
+          style={{
+            width: "90%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginTop: 20,
+            marginBottom: 20,
+          }}
+        >
+          <div
+            style={{
+              width: "50%",
+              alignItems: "center",
+              justifyContent: "flex-start",
+            }}
+          >
+            {" "}
+            <Typography variant="h2" style={{ fontFamily: "VT323" }}>
+              Transaction History
+            </Typography>
+          </div>
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#212121",
+              border: "5px solid #FFE227",
+              color: "white",
+              fontFamily: "VT323",
+              fontSize: 20,
+            }}
+            onClick={deleteReceipt}
+          >
+            {" "}
+            Reset History
+          </Button>
+        </div>
+
+        <div style={{ width: "90%", minHeight: "500px", paddingTop: 20 }}>
+          <TransactionTable receipt={receipt} />
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
