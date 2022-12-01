b49dbcf9111a245231073868f39f6c5b557e8248
Nic Amir <amir@besquare.com.my>
2022-12-01 05:41:59 +0000

    update

--- src/Pages/news.js
+++ src/Pages/news.js
@@ -1,5 +1,5 @@
 import getNews from "../service/newsservice";
-import { makeStyles, Typography } from "@material-ui/core";
+import { CircularProgress, makeStyles, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import moment from "moment/moment";
 import Pacman from "../asset/all-coins.gif";
@@ -115,89 +115,96 @@ function NewsPage() {
           </div>
         </div>
 
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            flexWrap: "wrap",
-            minHeight: "500px",
-          }}
-        >
-          {articles
-            .slice(0, show)
-            .sort(function (a, b) {
-              return new Date(b.datePublished) - new Date(a.datePublished);
-            })
-            .map((article, i) => {
-              return (
-                <div
-                  className={classes.newsbox}
-                  onClick={() => {
-                    window.open(article?.url);
-                  }}
-                  style={{ cursor: "pointer" }}
-                  key={i}
-                >
-                  <div>
-                    <div style={{ display: "flex", margin: 10 }}>
-                      <div style={{ marginRight: 20 }}>
-                        <img
-                          src={
-                            article?.image?.thumbnail?.contentUrl
-                              ? article?.image?.thumbnail?.contentUrl
-                              : "https://thumbs.dreamstime.com/b/forbidden-sign-no-pass-bypassing-icon-pixel-art-style-black-passing-bypass-unique-color-high-definition-perfect-web-149381794.jpg"
-                          }
-                          height={100}
-                        />
+        {articles ? (
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+              flexWrap: "wrap",
+              minHeight: "500px",
+            }}
+          >
+            {articles
+              .slice(0, show)
+              .sort(function (a, b) {
+                return new Date(b.datePublished) - new Date(a.datePublished);
+              })
+              .map((article, i) => {
+                return (
+                  <div
+                    className={classes.newsbox}
+                    onClick={() => {
+                      window.open(article?.url);
+                    }}
+                    style={{ cursor: "pointer" }}
+                    key={i}
+                  >
+                    <div>
+                      <div style={{ display: "flex", margin: 10 }}>
+                        <div style={{ marginRight: 20 }}>
+                          <img
+                            src={
+                              article?.image?.thumbnail?.contentUrl
+                                ? article?.image?.thumbnail?.contentUrl
+                                : "https://thumbs.dreamstime.com/b/forbidden-sign-no-pass-bypassing-icon-pixel-art-style-black-passing-bypass-unique-color-high-definition-perfect-web-149381794.jpg"
+                            }
+                            height={100}
+                          />
+                        </div>
+
+                        <Typography
+                          style={{ fontWeight: "bolder", color: "white" }}
+                        >
+                          {article?.name}
+                        </Typography>
                       </div>
 
                       <Typography
-                        style={{ fontWeight: "bolder", color: "white" }}
+                        style={{ color: "#F6F6F6", marginBottom: "revert" }}
                       >
-                        {article?.name}
+                        {article.description.length > 100
+                          ? `${article.description.substring(0, 100)}...`
+                          : article.description}
                       </Typography>
-                    </div>
-
-                    <Typography
-                      style={{ color: "#F6F6F6", marginBottom: "revert" }}
-                    >
-                      {article.description.length > 100
-                        ? `${article.description.substring(0, 100)}...`
-                        : article.description}
-                    </Typography>
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                      }}
-                    >
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        <img
-                          src={
-                            article.provider[0]?.image?.thumbnail?.contentUrl
-                              ? article.provider[0]?.image?.thumbnail
-                                  ?.contentUrl
-                              : "https://thumbs.dreamstime.com/b/forbidden-sign-no-pass-bypassing-icon-pixel-art-style-black-passing-bypass-unique-color-high-definition-perfect-web-149381794.jpg"
-                          }
-                          height={30}
-                          style={{ marginRight: 5, borderRadius: "50%" }}
-                        />
-                        <p>{article?.provider[0]?.name}</p>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          <img
+                            src={
+                              article.provider[0]?.image?.thumbnail?.contentUrl
+                                ? article.provider[0]?.image?.thumbnail
+                                    ?.contentUrl
+                                : "https://thumbs.dreamstime.com/b/forbidden-sign-no-pass-bypassing-icon-pixel-art-style-black-passing-bypass-unique-color-high-definition-perfect-web-149381794.jpg"
+                            }
+                            height={30}
+                            style={{ marginRight: 5, borderRadius: "50%" }}
+                          />
+                          <p>{article?.provider[0]?.name}</p>
+                        </div>
+                        <p style={{ color: "#FFE227" }}>
+                          {`${moment(article?.datePublished)
+                            .startOf("ss")
+                            .fromNow()}`}
+                        </p>
                       </div>
-                      <p style={{ color: "#FFE227" }}>
-                        {`${moment(article?.datePublished)
-                          .startOf("ss")
-                          .fromNow()}`}
-                      </p>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-        </div>
+                );
+              })}
+          </div>
+        ) : (
+          <div style={{ minHeight: "500px" }}>
+            {" "}
+            <CircularProgress />
+          </div>
+        )}
         <div
           style={{
             display: "flex",
